e4896b93abe7754a7c339823728d46a1597d7ed1
Victor Chukwuma <victorchuma1b3@gmail.com>
2025-05-16 22:08:54 +0100

    errorhandling

--- rpcEngine.js
+++ rpcEngine.js
@@ -1,25 +1,31 @@
-const { JsonRpcEngine } = require('json-rpc-engine');
-const { createAsyncMiddleware } = require('json-rpc-engine');
+const { JsonRpcEngine, createAsyncMiddleware } = require('json-rpc-engine');
 const fetch = require('node-fetch');
 
-const walletURL = process.env.INFURA_ID;
-if (!walletURL) {
+const infuraProjectId = process.env.INFURA_ID;
+if (!infuraProjectId) {
     throw new Error("INFURA_ID is not defined in environment variables");
 }
 
 const createEngine = () => {
     const engine = new JsonRpcEngine();
 
-    engine.push(createAsyncMiddleware(async (req, res, next) => {
-        const response = await fetch(`https://mainnet.infura.io/v3/${walletURL}`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(req),
-        });
+    engine.push(createAsyncMiddleware(async (req, res) => {
+        try {
+            const response = await fetch(`https://mainnet.infura.io/v3/${infuraProjectId}`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(req),
+            });
 
-        const json = await response.json();
-        Object.assign(res, json);
-        next();
+            const json = await response.json();
+            Object.assign(res, json);
+        } catch (error) {
+            res.error = {
+                code: -32603,
+                message: "Internal error: failed to fetch from Infura",
+                data: error.message,
+            };
+        }
     }));
 
     return engine;
